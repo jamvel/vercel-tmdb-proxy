e4ca79bc69d9a91e1b9f0680169e5f97f0560fd0
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-22 15:55:26 +0200

    Update return when no path is found in /api/tmdb

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,5 +1,6 @@
 const url = require("url");
 const axios = require("axios");
+const { version } = require("../package.json");
 
 const API_KEY = process.env.TMDB_API_KEY;
 
@@ -7,6 +8,14 @@ module.exports = async (req, res) => {
     try {
         // obtain url path and remove '/api/tmdb' base path
         const path = url.parse(req.url).pathname.replace('/api/tmdb', '');
+
+        if(path.length === 0 || path === '/'){
+            res.send({
+                'version': version,
+                'documentation': 'https://developers.themoviedb.org/3/getting-started/introduction',
+            })
+            return;
+        }
 
         const { data } = await axios({
             method: req.method,
